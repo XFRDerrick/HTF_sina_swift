8837f36de1589194687ac5ba3f007726c8bf8a2b
hetengfei <hetengfei163@126.com>
2015-12-12 22:29:15 +0800

    方法抽取添加控制器

--- sina_htf/sina_htf/Classes/Module/Main/MainViewController.swift
+++ sina_htf/sina_htf/Classes/Module/Main/MainViewController.swift
@@ -14,38 +14,30 @@ class MainViewController: UITabBarController {
         super.viewDidLoad()
 
         //添加子视图控制器
-        addChildViewController()
+        addChildViewControllers()
         
     }
 
-    private func addChildViewController() {
+    private func addChildViewControllers() {
+    
+        addChildViewController("首页", imageName: "tabbar_home", vc: HomeTableViewController())
+        addChildViewController("消息", imageName: "tabbar_message_center", vc: MessageTableViewController())
+        addChildViewController("发现", imageName: "tabbar_discover", vc: DiscoverTableViewController())
+        addChildViewController("我", imageName: "tabbar_profile", vc: ProfileTableViewController())
+    }
+    
+    private func addChildViewController(title:String , imageName:String,vc:UIViewController) {
+        
+        view.tintColor = UIColor.orangeColor()
+        //添加Tabbaritem Controller
+        let nav = UINavigationController(rootViewController: vc)
+        vc.title = title
+        vc.tabBarItem.image = UIImage(named: imageName)
         
         
-        //添加homeVC
-        
-        let homeVC = HomeTableViewController()
-        let nav = UINavigationController(rootViewController: homeVC)
-        homeVC.title = "首页"
         self.addChildViewController(nav)
         
-        //添加messageVC
-        let messageVC = MessageTableViewController()
-        let navMessage = UINavigationController(rootViewController: messageVC)
-        messageVC.title = "消息"
-        self.addChildViewController(navMessage)
-        
-        //添加discover
-        let discoverVC = DiscoverTableViewController()
-        let navDiscover = UINavigationController(rootViewController: discoverVC)
-        discoverVC.title = "发现"
-        self.addChildViewController(navDiscover)
-        
-        //添加profile
-        let profileVC = ProfileTableViewController()
-        let navProfile = UINavigationController(rootViewController: profileVC)
-        profileVC.title = "我"
-        self.addChildViewController(navProfile)
-        
     }
+    
     
 }
