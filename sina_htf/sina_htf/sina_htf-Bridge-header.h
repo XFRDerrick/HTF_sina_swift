6260caaca552dbe251576cd79b7503cabf9fc7d5
hetengfei <hetengfei163@126.com>
2016-01-03 21:58:19 +0800

    导入sina_htf-Bridge-header.h以及SDPhotoBrowser

---
+++ sina_htf/sina_htf/sina_htf-Bridge-header.h
@@ -0,0 +1,15 @@
+//
+//  sina_htf-Bridge-header.h
+//  sina_htf
+//
+//  Created by 赫腾飞 on 16/1/3.
+//  Copyright © 2016年 hetefe. All rights reserved.
+//
+
+#ifndef sina_htf_Bridge_header_h
+#define sina_htf_Bridge_header_h
+
+
+#endif /* sina_htf_Bridge_header_h */
+
+#import "SDPhotoBrowser.h"
