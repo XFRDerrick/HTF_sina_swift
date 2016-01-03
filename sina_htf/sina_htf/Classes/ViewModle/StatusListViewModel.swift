640f20f10499c09c5d722daeebd6e6bb898b49fa
hetengfei <hetengfei163@126.com>
2016-01-03 19:28:25 +0800

    单张图片的下载二

--- sina_htf/sina_htf/Classes/ViewModle/StatusListViewModel.swift
+++ sina_htf/sina_htf/Classes/ViewModle/StatusListViewModel.swift
@@ -11,6 +11,8 @@
 import UIKit
 
 import SVProgressHUD
+import SDWebImage
+
 class StatusListViewModel: NSObject {
 
     
@@ -54,13 +56,52 @@ class StatusListViewModel: NSObject {
                     list.append(s)
                     
                 }
+                //下载所有的单张图片 下载完之后进行回调
+                cacheStatusImage(list, finished: finished)
                 
                 //执行成功的回调
                 finished(statues: list)
 
             }
+ 
+        }
+    }
+    
+    class func cacheStatusImage(list: [Status], finished: (statues: [Status]?) -> () ) {
+        //下载所有的单张图片
+        //遍历数组
+        if list.count == 0 {
+            //一定要执行回调
+            finished(statues: list)
+            return
+        }
+        //创建群组对象 buffer 池子 将所有需要箭筒的异步任务放入到任务池中
+        let group = dispatch_group_create()
+        
+        
+        for s in list {
+           
+            if let urls = s.pictureURLs where  urls.count != 1 {
             
+                continue
+            }
+            //下载单张图片  等待所有的单张图片下载完毕再进行回调
+            //如何监听所有的异步任务完成 群组异步
+            //在下载之前将异步任务添加到任务池
+            //入组
+            dispatch_group_enter(group)
+            SDWebImageManager.sharedManager().downloadImageWithURL(s.pictureURLs?.last, options: [], progress: nil, completed: { (_, _, _, _, _) -> Void in
+                print("下载单张图片成功")
+                
+                //任务完成之后 必须出租  入组和出租是一一对应的有如有出
+                dispatch_group_leave(group)
+            })
             
+        }
+        //群组任务监听  对应的任务数量为零的时候 会触发监听方法
+        dispatch_group_notify(group, dispatch_get_main_queue()) { () -> Void in
+            //完成回调 
+            finished(statues: list)
         }
     }
     
